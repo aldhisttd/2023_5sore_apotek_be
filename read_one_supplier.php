32978aa47519c97d82a22673b980801b1b84cf6d
zaky10062001 <zakytjr5@gmail.com>
2023-12-10 17:14:41 +0300

    35.API Read By ID Supplier

---
+++ read_one_supplier.php
@@ -0,0 +1,31 @@
+<?php
+include 'env.php';
+
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => []
+    ]
+];
+
+$kode = $_GET['kode'];
+
+$result = mysqli_query($koneksi, "SELECT * FROM supplier WHERE kode = '$kode'");
+$rows = mysqli_num_rows($result);
+
+if($rows == 1){
+    while ($row = mysqli_fetch_assoc($result)){
+        $data = $row;
+    }
+    $response['status'] = 200;
+    $response['msg'] = 'success';
+    $response['body']['data'] = $data;
+} 
+if($rows == 0) {
+    $response['status'] = 400;
+    $response['msg'] = 'error';
+    $response['body']['data'] = $data;
+}
+
+echo json_encode($response);
