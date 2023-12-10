884ded4b6bab8bbd54c00c53d80b29931d2d7feb
Arini261203 <arini261203@gmail.com>
2023-12-10 19:00:20 +0700

    update supplier BE

---
+++ update_supplier.php
@@ -0,0 +1,26 @@
+<?php
+include 'env.php';
+
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => []
+    ]
+];
+
+$kode = $_POST['kode'];
+$nama = $_POST['nama'];
+$alamat = $_POST['alamat'];
+$hp = $_POST['hp'];
+
+mysqli_query($koneksi, "UPDATE supplier SET kode = '$kode', nama = '$nama', alamat = '$alamat', hp = '$hp' WHERE kode = '$kode'");
+
+$response['status'] = 200;
+$response['msg'] = 'data berhasil diubah';
+$response['body']['data']['kode'] = $kode;
+$response['body']['data']['nama'] = $nama;
+$response['body']['data']['alamat'] = $alamat;
+$response['body']['data']['hp'] = $hp;
+
+echo json_encode($response);
