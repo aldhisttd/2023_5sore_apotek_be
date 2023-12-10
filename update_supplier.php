5d790fb1ea694c604678ae851ca2514698e05846
Arini261203 <arini261203@gmail.com>
2023-12-10 19:34:40 +0700

    tambah pesan error

--- update_supplier.php
+++ update_supplier.php
@@ -14,13 +14,20 @@ $nama = $_POST['nama'];
 $alamat = $_POST['alamat'];
 $hp = $_POST['hp'];
 
-mysqli_query($koneksi, "UPDATE supplier SET kode = '$kode', nama = '$nama', alamat = '$alamat', hp = '$hp' WHERE kode = '$kode'");
+if(!isset($koneksi)){
 
-$response['status'] = 200;
-$response['msg'] = 'data berhasil diubah';
-$response['body']['data']['kode'] = $kode;
-$response['body']['data']['nama'] = $nama;
-$response['body']['data']['alamat'] = $alamat;
-$response['body']['data']['hp'] = $hp;
+    $response['status'] = 400;
+    $response['msg'] = 'data gagal diubah';    
+} else {
+
+    mysqli_query($koneksi, "UPDATE supplier SET kode = '$kode', nama = '$nama', alamat = '$alamat', hp = '$hp' WHERE kode = '$kode'");
+
+    $response['status'] = 200;
+    $response['msg'] = 'data berhasil diubah';
+    $response['body']['data']['kode'] = $kode;
+    $response['body']['data']['nama'] = $nama;
+    $response['body']['data']['alamat'] = $alamat;
+    $response['body']['data']['hp'] = $hp;        
+}
 
 echo json_encode($response);
