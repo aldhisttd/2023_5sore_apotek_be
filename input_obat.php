26b72d3efcb3627deba50d5d9c4cc232284530d8
ayukumaladewi <ayuk50362@gmail.com>
2023-12-11 17:33:21 +0700

    38.API Create Obat

--- input_obat.php
+++ input_obat.php
@@ -11,7 +11,6 @@ $response = [
 
 $kode = $_POST['kode'];
 $nama = $_POST['nama'];
-$gambar = $_POST['gambar'];
 $kode_kategori = $_POST['kode_kategori'];
 $kode_supplier = $_POST['kode_supplier'];
 $harga = $_POST['harga'];
@@ -26,16 +25,20 @@ if($row == 1){
     $response['msg'] = 'data sudah ada';
 } else {
 
+    $gambar = basename($_FILES["gambar"]["name"]);
+    $target_file = "upload/" . $gambar;
+    $upload = move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+    
     $response['status'] = 200;
     $response['msg'] = 'data berhasil ditambah';
     $response['body']['data']['kode'] = $kode;
     $response['body']['data']['nama'] = $nama;
-    $response['body']['data']['gambar'] = $gambar;
+    $response['body']['data']['gambar'] = 'upload/'.$gambar;
     $response['body']['data']['kode_kategori'] = $kode_kategori;
     $response['body']['data']['kode_supplier'] = $kode_supplier;
     $response['body']['data']['harga'] = $harga;
     
-    mysqli_query($koneksi, "INSERT INTO obat (kode, nama, gambar, kode_kategori, kode_supplier, harga) VALUES ('$kode', '$nama', '$gambar', '$kode_kategori', '$kode_supplier', '$harga')");
+    mysqli_query($koneksi, "INSERT INTO obat (kode, nama, gambar, kode_kategori, kode_supplier, harga) VALUES ('$kode', '$nama', 'upload/$gambar', '$kode_kategori', '$kode_supplier', '$harga')");
 }
 
 echo json_encode($response);
