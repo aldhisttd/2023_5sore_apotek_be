d681aa0978f42346965e94901ac8ce8725d4c211
Fahrilnurdian <fahrilnrdn03@gmail.com>
2023-12-12 11:25:32 +0700

    37.API Delete Supplier

---
+++ delete_supplier.php
@@ -0,0 +1,29 @@
+<?php
+include 'env.php';
+
+$response = [
+    'status' => '',
+    'msg' => '',
+    'body' => [
+        'data' => [
+            'kode' => ''
+        ]
+    ]
+];
+
+$kode = $_POST['kode'];
+
+if (!isset($koneksi)) {
+
+    $response['status'] = 400;
+    $response['msg'] = 'data gagal dihapus';
+    $response['body']['data']['kode'] = $kode;
+} else {
+    // ganti table obatnya
+    mysqli_query($koneksi, "DELETE FROM supplier WHERE kode = '$kode'");
+    $response['status'] = 200;
+    $response['msg'] = 'data berhasil dihapus';
+    $response['body']['data']['kode'] = $kode;
+}
+
+echo json_encode($response);
