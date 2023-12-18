8bf99f813ad2a1362f5a46b339cd83378a1595b4
GebyRezeki <gebydmi10@gmail.com>
2023-12-18 19:25:03 +0700

    hapus file

--- image.php
+++
@@ -1,9 +0,0 @@
-<?php
-$temp = explode(".", $_FILES["gambar"]["name"]);
-$namaGambarBaru = md5(date('dmy h:i:s')) . '.' . end($temp);
-// $nama_gambar = md5(date('dmy h:i:s'));
-
-$target_file = "upload/" . $namaGambarBaru;
-move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
-
-// mysqli_query($koneksi, "UPDATE obat SET gambar = 'upload/$gambarBaru' WHERE kode = '$kode'");
