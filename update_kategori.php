41ea3b937f966650013813ea5fd68afc5e00ef01
Fahrilnurdian <fahrilnrdn03@gmail.com>
2023-12-19 18:55:50 +0700

    31.API_update_kategori

--- update_kategori.php
+++ update_kategori.php
@@ -9,25 +9,21 @@ $response = [
     ]
 ];
 
-$kode = $_POST['kode'];
-$nama = $_POST['nama'];
-
 if(!isset($koneksi)){
    
     $response['status'] = 400;
     $response['msg'] = 'data gagal diubah';    
-
 } else {
 
+    $kode = $_POST['kode'];
+    $nama = $_POST['nama'];
 
-mysqli_query($koneksi, "UPDATE kategori SET kode = '$kode', nama = '$nama' WHERE kode = '$kode'");
+    $response['status'] = 200;
+    $response['msg'] = 'data berhasil diubah';
+    $response['body']['data']['kode'] = $kode;
+    $response['body']['data']['nama'] = $nama;
 
-$response['status'] = 200;
-$response['msg'] = 'data berhasil diubah';
-$response['body']['data']['kode'] = $kode;
-$response['body']['data']['nama'] = $nama;
-
+    mysqli_query($koneksi, "UPDATE kategori SET nama = '$nama' WHERE kode = '$kode'");
 }
-
 
 echo json_encode($response);
