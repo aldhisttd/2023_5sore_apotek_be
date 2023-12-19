4345ae3efa45345748131ee4d231c52f266ec468
ayukumaladewi <ayuk50362@gmail.com>
2023-12-19 18:33:31 +0700

    nama gambar

--- input_obat.php
+++ input_obat.php
@@ -9,11 +9,11 @@ $response = [
     ]
 ];
 
-$kode = $_POST['kode'];
-$nama = $_POST['nama'];
-$kode_kategori = $_POST['kode_kategori'];
-$kode_supplier = $_POST['kode_supplier'];
-$harga = $_POST['harga'];
+    $kode = $_POST['kode'];
+    $nama = $_POST['nama'];
+    $kode_kategori = $_POST['kode_kategori'];
+    $kode_supplier = $_POST['kode_supplier'];
+    $harga = $_POST['harga'];
 
 // cek duplikat
 $query = mysqli_query($koneksi, "SELECT * FROM obat WHERE kode = '$kode'");
@@ -25,22 +25,21 @@ if($row == 1){
     $response['msg'] = 'data sudah ada';
 } else {
 
-    $gambar = basename($_FILES["gambar"]["name"]);
-    $target_file = "upload/" . $gambar;
-    $upload = move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
-    
+    $temp = explode(".", $_FILES["gambar"]["name"]);
+    $namaGambarBaru = md5(date('dmy h:i:s')) . '.' . end($temp);
+    $target_file = "upload/" . $namaGambarBaru;
+    move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+
     $response['status'] = 200;
     $response['msg'] = 'data berhasil ditambah';
     $response['body']['data']['kode'] = $kode;
     $response['body']['data']['nama'] = $nama;
-    $response['body']['data']['gambar'] = 'upload/'.$gambar;
+    $response['body']['data']['gambar'] = 'upload/'.$namaGambarBaru;
     $response['body']['data']['kode_kategori'] = $kode_kategori;
     $response['body']['data']['kode_supplier'] = $kode_supplier;
     $response['body']['data']['harga'] = $harga;
-    
-    mysqli_query($koneksi, "INSERT INTO obat (kode, nama, gambar, kode_kategori, kode_supplier, harga) VALUES ('$kode', '$nama', 'upload/$gambar', '$kode_kategori', '$kode_supplier', '$harga')");
+
+    mysqli_query($koneksi, "INSERT INTO obat (kode, nama, gambar, kode_kategori, kode_supplier, harga) VALUES ('$kode', '$nama', 'upload/$namaGambarBaru', '$kode_kategori', '$kode_supplier', '$harga')");
 }
 
 echo json_encode($response);
-
-
