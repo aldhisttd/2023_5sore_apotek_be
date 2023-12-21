95425f22f72ccd3242bc855d8d76b704b8679399
ayukumaladewi <ayuk50362@gmail.com>
2023-12-21 12:40:58 +0700

    38.API_create_obat

--- image.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$temp = explode(".", $_FILES["gambar"]["name"]);
-$namaGambarBaru = md5(date('dmy h:i:s')) . '.' . end($temp);
-$target_file = "upload/" . $namaGambarBaru;
-move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
