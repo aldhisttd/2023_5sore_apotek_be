d2035c6076266f2b7ea0d348eb2f82df2e2d2013
Fahrilnurdian <fahrilnrdn03@gmail.com>
2024-01-01 20:12:19 +0700

    40.API Read by ID Obat

--- read_one_obat.php
+++ read_one_obat.php
@@ -17,9 +17,15 @@ if(!isset($koneksi)){
     $response['msg'] = 'error';
 }else{
     
-    $result = mysqli_query($koneksi, "SELECT * FROM obat WHERE kode = '$kode'");
-    $row = mysqli_fetch_assoc($result);
+    $query = "  SELECT obat.*, kategori.nama as nama_kategori, supplier.nama as nama_supplier
+                FROM Obat
+                INNER JOIN kategori ON obat.kode_kategori = kategori.kode
+                INNER JOIN supplier ON obat.kode_supplier = supplier.kode
+                WHERE obat.kode = '$kode'
+    ";
 
+    $result = mysqli_query($koneksi, $query);
+    $row = mysqli_fetch_assoc($result);
     $response['status'] = 200;
     $response['msg'] = 'success';
     $response['body']['data'] = $row;
